75624b153e740a313ec5b665978b5e505ec233c6
justenoneal <justenoneal@gmail.com>
2022-12-29 22:13:13 -0800

    rr5 done

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function GalleryItem(props) {
     let [viewDetails, setViewDetails] = useState(false)
 
     const simpleView = () => {
-        return(
+        return (
             <div>
                 <h3>{props.item.trackName}</h3>
                 <h4>{props.item.collectionName}</h4>
@@ -12,22 +13,41 @@ function GalleryItem(props) {
         )
     }
 
-    const detailedView = () => {
-        return(
-            <div>
-                <h3>{props.item.trackName}</h3>
-                <h4>{props.item.collectionName}</h4>
+    const detailStyle = {
+        'width': '80vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px',
+        'backgroundImage': `url(${props.item.artworkUrl100})`,
+        'backgroundRepeat': 'no-repeat',
+        'backgroundSize': 'cover',
+        'color': 'yellow'
+    }
+
+
+    const detailView = () => {
+        return (
+            <div style={detailStyle}>
+                <h2>{props.item.trackName}</h2>
+                <h3>
+                    <Link to={`/artist/${props.item.artistId}`}>
+                        {props.item.artistName}
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={`/album/${props.item.collectionId}`}>
+                        {props.item.collectionName}
+                    </Link>
+                </h3>
                 <h4>{props.item.primaryGenreName}</h4>
                 <h4>{props.item.releaseDate}</h4>
             </div>
         )
     }
 
-    return (
-        <div onClick={() => setViewDetails(!viewDetails)} style={{ display: "inline-block" }}>
-            {viewDetails ? detailedView() : simpleView}
-        </div>
-    )
 }
 
 export default GalleryItem
+
+
+
